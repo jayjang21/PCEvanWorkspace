8583dfb77d9c41d43667c985668b18ad6f0b17d0
NolanIS <nolanisaacsmith@gmail.com>
2015-02-09 11:48:24 -0800

    Added NButton

---
+++ PCEvanProject/src/NolanBackground/NButton.java
@@ -0,0 +1,24 @@
+package NolanBackground;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+public class NButton extends JButton{
+	
+	public NButton(String name){
+		this.setText(name);
+		this.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+			
+		});
+	}
+	
+	
+
+}
